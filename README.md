b20221914b358810c7460c1d2388a80994ebe3de
Erik Nielsen <erik@312development.com>
2015-08-18 08:12:40 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# jquery.scrollstart
+### jQuery scrollend
+
+A jQuery plugin that allows for window scroll-start event handling.
+
+## Demo
+
+See the demo <a href="http://nielse63.github.io/jquery.scrollend/" target="_blank">here</a>.
+
+## Usage
+
+Include both the jQuery library and the scrollend plugin in your project:
+
+```
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/jquery.scrollend.js"></script>
+```
+
+Execute the scrollend method to an element, instatiate options (if so desired), and run a callback functions:
+
+```js
+$(window).scrollend({
+	delay : 250
+}, function() {
+	// ...
+});
+```
+
+Or bind an element to the event, passing in options and event handler:
+
+```js
+$(window).on('scrollend', 250, function() {
+	// ...
+});
+```
+
+If you want to use the default delay setting of 250ms, simply don't include the options object:
+
+```js
+$(window).scrollend(function() {
+	// ...
+});
+```
+
+## Options
+
+The only option available is the delay time (in milliseconds) to execute the callback after the browser has stopped being resized. The default value is 250ms. This can be passed as either a number or object:
+
+```js
+// As a number
+$(window).on('scrollend', 250, function() {
+	// ...
+});
+
+// As an object
+$(window).on('scrollend', { delay : 250 }, function() {
+	// ...
+});
+```
+
+## License
+
+This plugin is licensed under the <a href="http://opensource.org/licenses/MIT" target="_blank">MIT license</a>.  A copy of the license is included in this package.
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/nielse63/jquery-scrollend/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
---
+++ dist/jquery.scrollend.js
@@ -0,0 +1,151 @@
+/*!
+ * jQuery scrollend - A jQuery plugin that allows for window scroll-start event handling.
+ * 
+ * Copyright (c) 2015 Erik Nielsen
+ * 
+ * Licensed under the MIT license:
+ *    http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Project home:
+ *    https://nielse63.github.io/jquery.scrollend/
+ * 
+ * Version:  0.2.0
+ * 
+ */
+
+(function(plugin) {
+	var chicago = window.Chicago || {
+		utils: {
+			now: Date.now || function() {
+				return new Date().getTime();
+			},
+			uid: function(prefix) {
+				return(prefix || "id") + chicago.utils.now() + "RAND" + Math.ceil(Math.random() * 1e5);
+			},
+			is: {
+				number: function(obj) {
+					return !isNaN(parseFloat(obj)) && isFinite(obj);
+				},
+				fn: function(obj) {
+					return typeof obj === "function";
+				},
+				object: function(obj) {
+					return Object.prototype.toString.call(obj) === "[object Object]";
+				}
+			},
+			debounce: function(fn, wait, immediate) {
+				var timeout;
+				return function() {
+					var context = this,
+						args = arguments,
+						later = function() {
+							timeout = null;
+							if(!immediate) {
+								fn.apply(context, args);
+							}
+						},
+						callNow = immediate && !timeout;
+					if(timeout) {
+						clearTimeout(timeout);
+					}
+					timeout = setTimeout(later, wait);
+					if(callNow) {
+						fn.apply(context, args);
+					}
+				};
+			}
+		},
+		$: window.jQuery || null
+	};
+	if(typeof define === "function" && define.amd) {
+		define("chicago", function() {
+			chicago.load = function(res, req, onload, config) {
+				var resources = res.split(","),
+					load = [];
+				var base = (config.config && config.config.chicago && config.config.chicago.base ? config.config.chicago.base : "").replace(/\/+$/g, "");
+				if(!base) {
+					throw new Error("Please define base path to jQuery scrollend in the requirejs config.");
+				}
+				var i = 0;
+				while(i < resources.length) {
+					var resource = resources[i].replace(/\./g, "/");
+					load.push(base + "/" + resource);
+					i += 1;
+				}
+				req(load, function() {
+					onload(chicago);
+				});
+			};
+			return chicago;
+		});
+	}
+	if(window && window.jQuery) {
+		return plugin(chicago, window, window.document);
+	} else {
+		if(!window.jQuery) {
+			throw new Error("jQuery scrollend requires jQuery");
+		}
+	}
+})(function(_c, win, doc) {
+	_c.$win = _c.$(win);
+	_c.$doc = _c.$(doc);
+	if(!_c.events) {
+		_c.events = {};
+	}
+	_c.events.scrollend = {
+		defaults: {
+			delay: 150
+		},
+		setup: function(data) {
+			var args = arguments,
+				options = {
+					delay: _c.$.event.special.scrollend.defaults.delay
+				},
+				fn;
+			if(_c.utils.is.fn(args[0])) {
+				fn = args[0];
+			} else {
+				if(_c.utils.is.number(args[0])) {
+					options.delay = args[0];
+				} else {
+					if(_c.utils.is.object(args[0])) {
+						options = _c.$.extend({}, options, args[0]);
+					}
+				}
+			}
+			var uid = _c.utils.uid("scrollend"),
+				_data = _c.$.extend({
+					delay: _c.$.event.special.scrollend.defaults.delay
+				}, options),
+				timer = null,
+				scrolling = false,
+				handler = function(e) {
+					if(timer) {
+						clearTimeout(timer);
+					} else {
+						e.type = "scrollend.chicago.dom";
+						_c.$(e.target).trigger("scrollend", e);
+					}
+					timer = setTimeout(function() {
+						timer = null;
+					}, _data.delay);
+				};
+			_c.$(this).data("chicago.event.scrollend.uid", uid);
+			return _c.$(this).on("scroll", _c.utils.debounce(handler, 100)).data(uid, handler);
+		},
+		teardown: function() {
+			var uid = _c.$(this).data("chicago.event.scrollend.uid");
+			_c.$(this).off("scroll", _c.$(this).data(uid));
+			_c.$(this).removeData(uid);
+			return _c.$(this).removeData("chicago.event.scrollend.uid");
+		}
+	};
+	(function() {
+		_c.$.event.special.scrollend = _c.events.scrollend;
+		_c.$.fn.scrollend = function(options, callback) {
+			return this.each(function() {
+				_c.$(this).on("scrollend", options, callback);
+			});
+		};
+	})();
+});
